f43ac077fa22192cb372287b8414905b05583851
longwatermelon <73869536+longwatermelon@users.noreply.github.com>
2021-02-01 10:10:52 -0700

    Useful comments to make it easier to understand the code

--- stringGenerator.py
+++ stringGenerator.py
@@ -1,10 +1,10 @@
-import random
+import random # We need to import random to pick a random character.
 
 
-def randomString(length:int, numberOfStrings= 1):
+def randomString(length:int, numberOfStrings= 1): # randomString definition
 
     string = [''.join([random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZY1234567890~!@#$%^&*()_+-=\'".,') for i in range(length)])
-             for i in range(numberOfStrings)]
+             for i in range(numberOfStrings)] # Generates a random sequence of strings
 
     return string
 #returns a list of the string(s)
